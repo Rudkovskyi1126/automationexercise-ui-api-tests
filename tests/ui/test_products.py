a8f7a165c9c4e87305a730a62a78b473799755a2
Alex <arudkovskyi@gmail.com>
2026-05-31 11:49:59 +0300

    feat: add TC-18 view category products and TC-19 view brand products tests

--- tests/ui/test_products.py
+++ tests/ui/test_products.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import expect
 from pages.products_page import ProductPage
+from pages.cart_page import CartPage
 
 
 def test_all_products_page(page):
@@ -28,4 +29,30 @@ def test_search_product(page):
     product_page.search_product()
     expect(page.locator(ProductPage.TITLE_SEARCH_PAGE)).to_be_visible()
     expect(page.locator(ProductPage.PRODUCT_CARD).first).to_be_visible()
+
+
+def test_view_category_products(page):
+    product_page = ProductPage(page)
+    expect(page.locator(ProductPage.LEFT_SIDEBAR_CATEGORY)).to_be_visible()
+    page.locator(ProductPage.WOMEN_CATEGORY).click()
+    page.locator(ProductPage.WOMEN_DRESS).click()
+    expect(page.locator(ProductPage.CATEGORY_TITLE)).to_contain_text("Women - Dress Products")
+    page.wait_for_load_state("domcontentloaded")
+    page.locator(ProductPage.MEN_CATEGORY).click()
+    page.locator(ProductPage.MEN_TSHIRTS).click()
+    expect(page.locator(ProductPage.CATEGORY_TITLE)).to_contain_text("Men - Tshirts Products")
+
+
+def test_view_brand_products(page):
+    product_page = ProductPage(page)
+    product_page.go_to_products_page()
+    page.locator(ProductPage.BRAND_POLO).click()
+    expect(page.locator(ProductPage.CATEGORY_TITLE)).to_contain_text("Polo")
+    page.locator(ProductPage.BRAND_HM).click()
+    expect(page.locator(ProductPage.CATEGORY_TITLE)).to_contain_text("H&M")
+    product_page.add_products_to_cart(1)
+    cart_page = CartPage(page)
+    cart_page.go_to_cart()
+    expect(page.locator(CartPage.CART_ITEM).first).to_be_visible()
+
 
